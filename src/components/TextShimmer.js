89b5d9861262f9335a677ef080c3e6a5d76ddcac
Aaditya <aaditya1@stanford.edu>
2025-04-15 11:07:52 -0700

    chore: remove legacy .js and unused files after TypeScript migration

--- src/components/TextShimmer.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import './TextShimmer.css';
-const TextShimmer = ({ text = "Thinking..." }) => {
-    return (_jsx("div", { className: "text-shimmer", children: _jsx("div", { className: "shimmer-text", children: text }) }));
-};
-export default TextShimmer;
